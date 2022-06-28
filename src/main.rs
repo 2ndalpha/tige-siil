be2d22a36358e9dffaa903ac4ce8560c9a98eb61
Siim Raud <siim@qminder.com>
2022-06-28 12:54:21 +0000

    Ugly music

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::error::Error;
+use std::{thread, time};
 
 fn listen_webhook_events() {
     let listener = TcpListener::bind("127.0.0.1:80").unwrap();
@@ -27,7 +28,7 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("Request: {}", parts[parts.len() - 1]);
     respond_ok(stream);
-    send_midi();
+    play_new_ticket();
 }
 
 fn respond_ok(mut stream: TcpStream) {
@@ -44,11 +45,42 @@ fn respond_bad_method(mut stream: TcpStream) {
     stream.flush().unwrap();
 }
 
-fn send_note() -> Result<(), Box<dyn Error>> {
+fn play_new_ticket() {
+    play_note(80);
+    delay(500);
+
+    play_note(52);
+    delay(500);
+
+    play_note(40);
+
+    delay(2000);
+
+    stop_note(80);
+    stop_note(52);
+    stop_note(40);
+}
+
+fn delay(mut milliseconds: u64) {
+    thread::sleep(time::Duration::from_millis(milliseconds));
+}
+
+fn play_note(mut note: u8) -> Result<(), Box<dyn Error>> {
     let client = reqwest::blocking::Client::new();
     let resp = client.post("https://fb72-195-250-172-87.eu.ngrok.io/")
     .header("Content-Type", "application/json")
-    .body("{\"note\": 52, \"velocity\": 20, \"channel\": 0, \"isOn\": true }")
+    .body(format!("{{\"note\": {note}, \"velocity\": 20, \"channel\": 0, \"isOn\": true }}"))
+    .send()?;
+
+    println!("{:#?}", resp);
+    Ok(())
+}
+
+fn stop_note(mut note: u8) -> Result<(), Box<dyn Error>> {
+    let client = reqwest::blocking::Client::new();
+    let resp = client.post("https://fb72-195-250-172-87.eu.ngrok.io/")
+    .header("Content-Type", "application/json")
+    .body(format!("{{\"note\": {note}, \"velocity\": 20, \"channel\": 0, \"isOn\": false }}"))
     .send()?;
 
     println!("{:#?}", resp);
