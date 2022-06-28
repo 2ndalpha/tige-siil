869ad8491d738aa9e40189bbb2c6aa01c46d9b76
Siim Raud <siim@qminder.com>
2022-06-28 11:18:07 +0000

    Fixed the codespaces

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,5 @@
+set -eu
+
 ## update and install some things we should probably have
 apt-get update
 apt-get install -y \
@@ -12,12 +14,8 @@ apt-get install -y \
   openssl
 
 ## Install rustup and common components
-curl https://sh.rustup.rs -sSf | sh -s -- -y 
-rustup install nightly
-rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
-rustup component add clippy 
-rustup component add clippy --toolchain nightly
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+export PATH=/root/.cargo/bin:$PATH
 
-cargo install cargo-expand
-cargo install cargo-edit
+rustup component add rustfmt
+rustup component add clippy
