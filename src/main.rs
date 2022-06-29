b904931f6071e1e3099072654313615583ec886a
Siim Raud <siim@qminder.com>
2022-06-29 06:07:32 +0000

    Day 1

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::net::TcpStream;
 use std::{thread, time};
 
 use threadpool::ThreadPool;
+use tokio::runtime::Builder;
+use tokio::spawn;
 
 fn listen_webhook_events() {
     let listener = TcpListener::bind("127.0.0.1:80").unwrap();
@@ -18,7 +20,7 @@ fn listen_webhook_events() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
@@ -36,7 +38,8 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: {}", payload);
 
     if payload.contains("ticket_created") {
-        play_drums();
+        //play_drums();
+        play_mario();
     }
     if payload.contains("ticket_changed") {
         play_fast_drums();
@@ -66,6 +69,83 @@ fn respond_bad_method(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+async fn play_mario() {
+    let part1 = [
+        76, 12, 76, 12, 20, 12, 76, 12, 20, 12, 72, 12, 76, 12, 20, 12, 79, 12, 20, 36, 67, 12, 20,
+        36,
+    ];
+    let part2 = [
+        72, 12, 20, 24, 67, 12, 20, 24, 64, 12, 20, 24, 69, 12, 20, 12, 71, 12, 20, 12, 70, 12, 69,
+        12, 20, 12, 67, 16, 76, 16, 79, 16, 81, 12, 20, 12, 77, 12, 79, 12, 20, 12, 76, 12, 20, 12,
+        72, 12, 74, 12, 71, 12, 20, 24,
+    ];
+    let part3 = [
+        48, 12, 20, 12, 79, 12, 78, 12, 77, 12, 75, 12, 60, 12, 76, 12, 53, 12, 68, 12, 69, 12, 72,
+        12, 60, 12, 69, 12, 72, 12, 74, 12, 48, 12, 20, 12, 79, 12, 78, 12, 77, 12, 75, 12, 55, 12,
+        76, 12, 20, 12, 84, 12, 20, 12, 84, 12, 84, 12,
+    ];
+    let part4 = [
+        55, 12, 20, 12, 48, 12, 20, 12, 79, 12, 78, 12, 77, 12, 75, 12, 60, 12, 76, 12, 53, 12, 68,
+        12, 69, 12, 72, 12, 60, 12, 69, 12, 72, 12, 74, 12, 48, 12, 20, 12, 75, 24, 20, 12, 74, 24,
+        20, 12, 72, 24, 20, 12, 55, 12, 55, 12, 20, 12, 48, 12,
+    ];
+    let part5 = [
+        72, 12, 72, 12, 20, 12, 72, 12, 20, 12, 72, 12, 74, 12, 20, 12, 76, 12, 72, 12, 20, 12, 69,
+        12, 67, 12, 20, 12, 43, 12, 20, 12, 72, 12, 72, 12, 20, 12, 72, 12, 20, 12, 72, 12, 74, 12,
+        76, 12, 55, 12, 20, 24, 48, 12, 20, 24, 43, 12, 20, 12, 72, 12, 72, 12, 20, 12, 72, 12, 20,
+        12, 72, 12, 74, 12, 20, 12, 76, 12, 72, 12, 20, 12, 69, 12, 67, 12, 20, 12, 43, 12, 20, 12,
+        76, 12, 76, 12, 20, 12, 76, 12, 20, 12, 72, 12, 76, 12, 20, 12, 79, 12, 20, 36, 67, 12, 20,
+        36,
+    ];
+    let part6 = [
+        76, 12, 72, 12, 20, 12, 67, 12, 55, 12, 20, 12, 68, 12, 20, 12, 69, 12, 77, 12, 53, 12, 77,
+        12, 69, 12, 60, 12, 53, 12, 20, 12, 71, 16, 81, 16, 81, 16, 81, 16, 79, 16, 77, 16, 76, 12,
+        72, 12, 55, 12, 69, 12, 67, 12, 60, 12, 55, 12, 20, 12, 76, 12, 72, 12, 20, 12, 67, 12, 55,
+        12, 20, 12, 68, 12, 20, 12, 69, 12, 77, 12, 53, 12, 77, 12, 69, 12, 60, 12, 53, 12, 20, 12,
+        71, 12, 77, 12, 20, 12, 77, 12, 77, 16, 76, 16, 74, 16, 72, 12, 64, 12, 55, 12, 64, 12, 60,
+        12, 20, 36,
+    ];
+    let part7 = [
+        72, 12, 20, 24, 67, 12, 20, 24, 64, 24, 69, 16, 71, 16, 69, 16, 68, 24, 70, 24, 68, 24, 67,
+        12, 65, 12, 67, 48,
+    ];
+
+    play_mario_notes(&part1);
+
+    play_mario_notes(&part2);
+    play_mario_notes(&part2);
+
+    let part3_join_handle = spawn(async move {
+        play_mario_notes(&part3);
+    });
+    let part4_join_handle = spawn(async move {
+        play_mario_notes(&part4);
+    });
+
+    part3_join_handle.await;
+    part4_join_handle.await;
+
+    play_mario_notes(&part5);
+
+    play_mario_notes(&part2);
+    play_mario_notes(&part2);
+
+    play_mario_notes(&part6);
+    play_mario_notes(&part6);
+
+    play_mario_notes(&part5);
+    play_mario_notes(&part6);
+    play_mario_notes(&part7);
+}
+
+fn play_mario_notes(notes: &[usize]) {
+    for note in notes {
+        play_note(*note, 1, 127);
+        delay(80);
+        stop_note(*note, 1);
+    }
 }
 
 // Channel for drums: 10
@@ -118,36 +198,53 @@ fn delay(milliseconds: usize) {
 }
 
 fn play_note(note: usize, channel: usize, velocity: usize) {
-    let client = reqwest::blocking::Client::new();
-    let result = client
-        .post("http://ec2-13-48-30-252.eu-north-1.compute.amazonaws.com:3000/")
-        .header("Content-Type", "application/json")
-        .body(format!(
-            "{{\"note\": {note}, \"velocity\": {velocity}, \"channel\": {channel}, \"isOn\": true }}"
-        ))
-        .send();
-    match result {
-        Err(e) => println!("Failed to start the note{:?}", e),
-        _ => (),
-    }
+    let client = reqwest::Client::new();
+    spawn(async move {
+        println!("Playing {note}");
+        let result = client
+            .post("http://ec2-13-48-30-252.eu-north-1.compute.amazonaws.com:3000/")
+            .header("Content-Type", "application/json")
+            .body(format!(
+                "{{\"note\": {note}, \"velocity\": {velocity}, \"channel\": {channel}, \"isOn\": true }}"
+            ))
+            .send().await;
+        match result {
+            Err(e) => println!("Failed to start the note{:?}", e),
+            _ => (),
+        }
+    });
 }
 
 fn stop_note(note: usize, channel: usize) {
-    let client = reqwest::blocking::Client::new();
-    let result = client
-        .post("http://ec2-13-48-30-252.eu-north-1.compute.amazonaws.com:3000/")
-        .header("Content-Type", "application/json")
-        .body(format!(
-            "{{\"note\": {note}, \"velocity\": 0, \"channel\": {channel}, \"isOn\": false }}"
-        ))
-        .send();
-    match result {
-        Err(e) => println!("Failed to stop the note{:?}", e),
-        _ => (),
-    }
+    let client = reqwest::Client::new();
+    spawn(async move {
+        let result = client
+            .post("http://ec2-13-48-30-252.eu-north-1.compute.amazonaws.com:3000/")
+            .header("Content-Type", "application/json")
+            .body(format!(
+                "{{\"note\": {note}, \"velocity\": 127, \"channel\": {channel}, \"isOn\": false }}"
+            ))
+            .send()
+            .await;
+        match result {
+            Err(e) => println!("Failed to stop the note{:?}", e),
+            _ => (),
+        }
+    });
 }
 
 fn main() {
     println!("🦔 Let's make some noise!");
-    listen_webhook_events();
+
+    let runtime = Builder::new_multi_thread()
+        .worker_threads(20)
+        .thread_stack_size(3 * 1024 * 1024)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    runtime.block_on(async move {
+        play_mario().await;
+        listen_webhook_events();
+    });
 }
